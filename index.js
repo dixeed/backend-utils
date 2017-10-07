651c569f9ef767a6d57ff5e0b08f7401b9442beb
Timothé BORDIGA <t.bordiga@dixeed.com>
2017-10-07 20:49:25 +0200

    fix(utils-rework): Changes exported module + corrects bugs in media

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 'use strict';
 
-module.exports = config => {
-  const main = (module.exports = {});
+const validate = require('./lib/validate');
+const media = require('./lib/media');
+const crypto = require('./lib/crypto');
+const Mailer = require('./lib/mailer');
+const templater = require('./lib/templater');
+const Hoek = require('hoek');
 
-  main.validate = require('./lib/validate');
-  main.media = require('./lib/media')(config.media);
-  main.crypto = require('./lib/crypto')(config.algorythm);
-  main.Mailer = require('./lib/mailer');
-  main.templater = require('./lib/templater');
+let initialized = false;
 
-  return main;
+module.exports = {
+  init(config) {
+    Hoek.assert(initialized === false, 'You should call init() only once.');
+
+    this.validate = validate;
+    this.media = media(config.media);
+    this.crypto = crypto(config.algorythm);
+    this.Mailer = Mailer;
+    this.templater = templater;
+
+    initialized = true;
+  },
 };
